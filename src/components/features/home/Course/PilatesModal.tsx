21e0f12d77171147f220363e9a67f970948d588e
ValentinG <valentin.guillot@stellia.ai>
2025-02-16 22:06:36 +0100

    feat(home): update texts & add rotating text section

--- src/components/features/home/Course/PilatesModal.tsx
+++ src/components/features/home/Course/PilatesModal.tsx
@@ -10,9 +10,11 @@ const PilatesModal = ({ isOpen, handleCloseClick }: PilatesModalProps) => {
 		<Modal isOpen={isOpen} handleCloseClick={handleCloseClick}>
 			<h1 className="mb-6 text-2xl">Pilates</h1>
 			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit laudantium ullam enim, ea natus quaerat
-				neque nobis voluptates temporibus! Debitis natus doloremque quaerat itaque rem autem libero eligendi quia
-				mollitia?
+				Créée par Joseph Pilates en 1926, le Pilates connaît beaucoup de succès auprès des danseurs et athlètes. <br />A
+				partir de 1968 la méthode ne cesse de se développer à travers le monde. <br />
+				Cette méthode favorise le travail de l’intérieur vers l’extérieur, il faut commencer par renforcer les muscles
+				stabilisateurs, profonds du tronc pendant que les muscles du mouvement s’activent. <br /> De ce fait le Pilates
+				développe tous les muscles et de façon harmonieuse.
 			</p>
 		</Modal>
 	)
