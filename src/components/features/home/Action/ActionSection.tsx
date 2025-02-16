2eb6c5da2f0ec84dbe3b558c7d10509b0b1a389f
ValentinG <valentin.guillot@stellia.ai>
2025-02-16 10:27:22 +0100

    feat(home): Add tel link to Action section

--- src/components/features/home/Action/ActionSection.tsx
+++ src/components/features/home/Action/ActionSection.tsx
@@ -1,16 +1,20 @@
 import BlurText from '@/components/ui/BlurText'
 import ClickSpark from '@/components/ui/ClickSpark'
+import Link from 'next/link'
 
 const ActionSection = () => {
+	const tel = typeof window !== undefined ? 'tel:+336' : '#cta'
 	return (
 		<section id="cta" className="bg-base-200 relative flex h-[50svh] items-center justify-center">
-			<BlurText
-				text="Réservez votre séance d’essai au 06.49.81.02.80"
-				delay={50}
-				animateBy="words"
-				direction="bottom"
-				className="text-base-content font-heading flex max-w-3/5 justify-center text-4xl leading-30 md:text-6xl"
-			/>
+			<Link className="flex w-fit justify-center" href={tel}>
+				<BlurText
+					text="Réservez votre séance d’essai au 06.49.81.02.80"
+					delay={50}
+					animateBy="words"
+					direction="bottom"
+					className="text-base-content font-heading relative z-1 flex max-w-3/5 justify-center text-4xl leading-30 md:text-6xl"
+				/>
+			</Link>
 			<ClickSpark sparkColor="oklch(44% 0.011 73.639)" sparkSize={10} sparkRadius={20} sparkCount={8} duration={400} />
 		</section>
 	)
