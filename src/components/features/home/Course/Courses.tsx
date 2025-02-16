12824e925972cccfd3f68a31bf000e38fe87e911
ValentinG <valentin.guillot@stellia.ai>
2025-02-16 22:23:11 +0100

    fix(home): move rotating text

--- src/components/features/home/Course/Courses.tsx
+++ src/components/features/home/Course/Courses.tsx
@@ -1,4 +1,5 @@
 import Card from '@/components/ui/Card'
+import RotatingText from '@/components/ui/RotatingText'
 import TiltedCard from '@/components/ui/TiltedCard'
 import useIsMobile from '@/hooks/shared/useIsMobile'
 import Image from 'next/image'
@@ -29,6 +30,29 @@ const Courses = ({ handleClicks }: CourseProps) => {
 				<div className="to-neutral rounded-box absolute top-0 h-full w-full bg-gradient-to-b from-transparent via-transparent" />
 				<p className="font-heading text-neutral-content absolute bottom-5 left-5 text-xl font-bold">Pilates</p>
 			</Card>
+			<div className="flex flex-col items-center gap-4 md:flex-row md:gap-2">
+				<p className="text-xl">Les principes du Pilates </p>
+				<RotatingText
+					texts={[
+						'RESPIRATION',
+						'CONTRÔLE',
+						'CENTRAGE',
+						'CONCENTRATION',
+						'PRÉCISION',
+						'FLUIDITÉ',
+						'ROUTINE',
+						'ÉQUILIBRE',
+					]}
+					rotationInterval={5000}
+					staggerDuration={0.025}
+					transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+					initial={{ y: '100%' }}
+					animate={{ y: 0 }}
+					exit={{ y: '-120%' }}
+					mainClassName="px-3 md:px-3 bg-neutral text-neutral-content overflow-hidden py-1 md:py-2 justify-center rounded-box font-heading font-bold text-2xl"
+					splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+				/>
+			</div>
 			<Card className="relative" onClick={handleCardioClick}>
 				<Image
 					src={cardioImgUrl}
@@ -47,40 +71,65 @@ const Courses = ({ handleClicks }: CourseProps) => {
 	)
 
 	const webContent = (
-		<div className="flex w-full justify-between">
-			<TiltedCard
-				imageSrc={pilatesImgUrl}
-				altText={pilatesAlt}
-				showTooltip={false}
-				containerHeight="100%"
-				imageHeight={'500px'}
-				imageWidth={'500px'}
-				displayOverlayContent
-				scaleOnHover={1}
-				overlayContent={
-					<p className="font-heading text-neutral-content bg-neutral/90 rounded-box px-6 py-4 text-center text-xl font-bold">
-						Pilates
-					</p>
-				}
-				onClick={handlePilatesClick}
-			/>
-			<TiltedCard
-				imageSrc={cardioImgUrl}
-				altText={cardioAlt}
-				showTooltip={false}
-				containerHeight="100%"
-				imageHeight={'500px'}
-				imageWidth={'500px'}
-				scaleOnHover={1}
-				displayOverlayContent
-				overlayContent={
-					<p className="font-heading text-neutral-content bg-neutral/90 rounded-box px-6 py-4 text-center text-xl font-bold">
-						Cardio / Renforcement Musculaire
-					</p>
-				}
-				onClick={handleCardioClick}
-			/>
-		</div>
+		<>
+			<div className="flex w-full justify-between">
+				<TiltedCard
+					imageSrc={pilatesImgUrl}
+					altText={pilatesAlt}
+					showTooltip={false}
+					containerHeight="100%"
+					imageHeight={'500px'}
+					imageWidth={'500px'}
+					displayOverlayContent
+					scaleOnHover={1}
+					overlayContent={
+						<p className="font-heading text-neutral-content bg-neutral/90 rounded-box px-6 py-4 text-center text-xl font-bold">
+							Pilates
+						</p>
+					}
+					onClick={handlePilatesClick}
+				/>
+				<TiltedCard
+					imageSrc={cardioImgUrl}
+					altText={cardioAlt}
+					showTooltip={false}
+					containerHeight="100%"
+					imageHeight={'500px'}
+					imageWidth={'500px'}
+					scaleOnHover={1}
+					displayOverlayContent
+					overlayContent={
+						<p className="font-heading text-neutral-content bg-neutral/90 rounded-box px-6 py-4 text-center text-xl font-bold">
+							Cardio / Renforcement Musculaire
+						</p>
+					}
+					onClick={handleCardioClick}
+				/>
+			</div>
+			<div className="flex flex-col items-center gap-4 md:flex-row md:gap-2">
+				<p className="text-xl">Les principes du Pilates </p>
+				<RotatingText
+					texts={[
+						'RESPIRATION',
+						'CONTRÔLE',
+						'CENTRAGE',
+						'CONCENTRATION',
+						'PRÉCISION',
+						'FLUIDITÉ',
+						'ROUTINE',
+						'ÉQUILIBRE',
+					]}
+					rotationInterval={5000}
+					staggerDuration={0.025}
+					transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+					initial={{ y: '100%' }}
+					animate={{ y: 0 }}
+					exit={{ y: '-120%' }}
+					mainClassName="px-3 md:px-3 bg-neutral text-neutral-content overflow-hidden py-1 md:py-2 justify-center rounded-box font-heading font-bold text-2xl"
+					splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+				/>
+			</div>
+		</>
 	)
 
 	return isMobile ? mobileContent : webContent
