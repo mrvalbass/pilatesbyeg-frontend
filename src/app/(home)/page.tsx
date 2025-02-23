5ff0c0c593f4c1920b1a810ceaecd8d623352d88
ValentinG <valentin.guillot@stellia.ai>
2025-02-23 12:00:32 +0100

    feat(home): update Action Scetion according to customer request

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,8 @@
 import AboutSection from '@/components/features/home/About/AboutSection'
-import ActionSection from '@/components/features/home/Action/ActionSection'
 import CourseSection from '@/components/features/home/Course/CourseSection'
 import HeroSection from '@/components/features/home/Hero/HeroSection'
 import ReviewSection from '@/components/features/home/Review/ReviewSection'
+import ActionSection from '@/components/shared/Action/ActionSection'
 
 function Home() {
 	return (
